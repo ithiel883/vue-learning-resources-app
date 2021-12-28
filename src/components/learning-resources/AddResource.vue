b3ccbc53134d21ecf79639ea7a7531d39a1c7092
giranPaladin <ajuka.zephiniah@gmail.com>
2021-12-28 14:38:06 +0100

    added final features and delete button

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,25 +1,69 @@
 <template>
-   <base-card>
-   <form>
-    <div class="form-control">
-       <label for="title">Title</label>
-       <input id="title" name="title" type="text">
-    </div>
-    <div class="form-control">
-       <label for="description">Description</label>
-    <textarea id="description" name="description" rows="3"></textarea>
-    </div>
-     <div class="form-control">
-       <label for="link">Link</label>
-       <input id="link" name="link" type="url">
-    </div>
-    <div>
-        <base-button type="submit">Add Resource</base-button>
-    </div>
-   </form>
-   </base-card>
-</template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input data is invalid</p>
+      <p>Make sure you enter at least a few characters into each input fields</p>
+    </template>
+    <template #actions>
+<base-button @click="confirmError">Okay</base-button>
 
+    </template>
+  </base-dialog>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" ref="titleInput" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          rows="3"
+          ref="descInput"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="url" ref="linkInput" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
+</template>
+<script>
+export default {
+  inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredUrl = this.$refs.linkInput.value;
+
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredUrl.trim()
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+      this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
 <style scoped>
 label {
   font-weight: bold;
--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -12,7 +12,9 @@
       >Add Resources</base-button
     >
   </base-card>
+  <keep-alive>
   <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 
@@ -28,6 +30,8 @@ export default {
   provide() {
     return {
       resources: this.storedResources,
+      addResource: this.addResource,
+      deleteResource: this.removeResource
     };
   },
   data() {
@@ -61,6 +65,20 @@ export default {
     setSelectedTab(tab) {
       this.selectedTab = tab;
     },
+    addResource(title, description, url) {
+        const newResource = {
+            id: new Date().toISOString(),
+            title: title,
+            description: description,
+            link: url
+        }
+        this.storedResources.unshift(newResource)
+        this.selectedTab = 'stored-resources'
+    },
+    removeResource(resId) {
+       const resIndex = this.storedResources.findIndex(res => res.id === resId);
+       this.storedResources.splice(resIndex, 1)
+    }
   },
 };
 </script>
